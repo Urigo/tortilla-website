25c5c48203c3770f54d6fd032aca203ec12f4b4c
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-03-27 09:05:43 +0200

    Fix components names

--- src/components/common/Bookmarks.tsx
+++ src/components/common/Bookmarks.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { faBookmark } from '@fortawesome/fontawesome-free-regular'
 
-import FontIcon from './Toolbar/FontIcon'
+import ToolbarFontIcon from './Toolbar/FontIcon'
 
-export default () => <FontIcon icon={faBookmark} />
+export default () => <ToolbarFontIcon icon={faBookmark} />
--- src/components/common/Notifications.tsx
+++ src/components/common/Notifications.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { faBell } from '@fortawesome/fontawesome-free-regular'
 
-import FontIcon from './Toolbar/FontIcon'
+import ToolbarFontIcon from './Toolbar/FontIcon'
 
-export default () => <FontIcon icon={faBell} />
+export default () => <ToolbarFontIcon icon={faBell} />
