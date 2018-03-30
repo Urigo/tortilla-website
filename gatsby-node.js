0953d15066f04309f27f443ebf3bc43fdb385bc7
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-03-30 19:23:20 +0200

    Do not load react-modal in Node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,12 @@
 const createPages = require('./src/features/create-pages')
 
 exports.createPages = createPages
+
+exports.modifyWebpackConfig = ({ config, stage }) => {
+  if (stage === 'build-html') {
+    config.loader('null', {
+      test: /react-modal/,
+      loader: 'null-loader',
+    })
+  }
+}
