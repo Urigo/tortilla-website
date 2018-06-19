7036b18dfe217155bc459e6af19ae057e35580cf
DAB0mB <emanor6@gmail.com>
2018-06-19 11:42:43 +0800

    Dynamically build diff view

--- src/components/tutorial/Contents/DiffContent.js
+++ src/components/tutorial/Contents/DiffContent.js
@@ -1,6 +1,7 @@
 import 'react-diff-view/index.css'
 
 import React from 'react'
+import ReactDOM from 'react-dom'
 import styled from 'styled-components'
 
 import { parseDiff, Diff as ReactDiffView } from '../../../libs/react-diff-view'
@@ -167,6 +168,10 @@ export default class extends React.Component {
     this.parseDiff()
   }
 
+  componentDidMount() {
+    this.buildDiff()
+  }
+
   componentWillUpdate(props, state) {
     this.resetViewTypeParams(state)
   }
@@ -229,103 +234,116 @@ export default class extends React.Component {
       <Content>
         <Title>$ tortilla release diff {this.props.destVersion} {this.props.srcVersion}</Title>
         <ViewTypeButton onClick={this.toggleDiffViewType}>{this.viewTypeAction}</ViewTypeButton>
-
-        {this.renderDiff()}
+        <Diff ref={ref => this.diffContainer = ReactDOM.findDOMNode(ref)} />
       </Content>
     )
   }
 
-  renderDiff() {
+  buildDiff() {
+    // DOM node not found
+    if (!this.diffContainer) return
+
+    // Rendering file diffs parallely
+    const renderings = this.files.map((file, i) => new Promise(resolve => {
+      ReactDOM.render(
+        this.renderDiffFile(file, i, resolve),
+        document.createElement('span'),
+      )
+    }))
+
+    // Appending each one at a time in series
+    renderings.reduce((rendered, rendering) => rendered.then(ref => {
+      this.diffContainer.appendChild(ref)
+
+      return rendering
+    }))
+  }
+
+  renderDiffFile({ oldPath, newPath, hunks, isBinary }, i, callback) {
+    let header = []
+
+    // File removed
+    if (oldPath != '/dev/null') {
+      header.push(this.destBaseUrl
+        ? <Path key={0} href={`${this.destBaseUrl}/${oldPath}`}>{oldPath}</Path>
+        : <NullPath key={0}>{oldPath}</NullPath>
+      )
+    }
+
+    if (newPath != '/dev/null') {
+      // File changed
+      if (newPath == oldPath) {
+        header = [this.srcBaseUrl
+          ? <Path key={0} href={`${this.srcBaseUrl}/${oldPath}`}>{oldPath}</Path>
+          : <NullPath key={0}>{oldPath}</NullPath>
+        ]
+      // File renamed or added
+      } else {
+        header.push(this.srcBaseUrl
+          ? <Path key={header.length} href={`${this.srcBaseUrl}/${newPath}`}>{newPath}</Path>
+          : <NullPath key={header.length}>{newPath}</NullPath>
+        )
+      }
+    }
+
+    if (header.length == 2) {
+      header.splice(1, 0, <span key={0.5}>→</span>)
+    }
+
+    /* Adding 2 for padding of 1ch in each side */
+    const gutterWidth = this.maxLineNums[i] + 2;
+    const lineWidth = this.maxLineLengths[i] + 1;
+
+    const Container = styled.span`
+      margin: 20px;
+      display: block;
+      border: 1px solid silver;
+      border-radius: 3px;
+
+      .diff-hunk {
+        width: ${this.diffHunkWidth}%;
+      }
+
+      .diff-hunk-header-gutter {
+        width: ${gutterWidth * this.gutterProduct - (0.4 * this.gutterProduct)}ch;
+      }
+
+      .diff-hunk-header {
+        width: ${this.state.diffViewType == 'split' && '200%'};
+      }
+
+      .diff-gutter {
+        width: ${gutterWidth}ch;
+      }
+
+      .diff-code, .diff-hunk-header-content {
+        min-width: calc(100% - ${gutterWidth * this.gutterProduct}ch);
+        width: ${lineWidth}ch;
+      }
+
+      .diff-binary {
+        width: 100%;
+        padding: 0;
+        text-align: center;
+        font-weight: bold;
+      }
+
+      .diff-hunk-header-content {
+        width: ${lineWidth * (2 / this.gutterProduct) + gutterWidth}ch;
+      }
+    `
+
     return (
-      <Diff>
-        {this.files.map(({ oldPath, newPath, hunks, isBinary }, i) => {
-          let header = []
-
-          // File removed
-          if (oldPath != '/dev/null') {
-            header.push(this.destBaseUrl
-              ? <Path key={0} href={`${this.destBaseUrl}/${oldPath}`}>{oldPath}</Path>
-              : <NullPath key={0}>{oldPath}</NullPath>
-            )
-          }
-
-          if (newPath != '/dev/null') {
-            // File changed
-            if (newPath == oldPath) {
-              header = [this.srcBaseUrl
-                ? <Path key={0} href={`${this.srcBaseUrl}/${oldPath}`}>{oldPath}</Path>
-                : <NullPath key={0}>{oldPath}</NullPath>
-              ]
-            // File renamed or added
-            } else {
-              header.push(this.srcBaseUrl
-                ? <Path key={header.length} href={`${this.srcBaseUrl}/${newPath}`}>{newPath}</Path>
-                : <NullPath key={header.length}>{newPath}</NullPath>
-              )
-            }
-          }
-
-          if (header.length == 2) {
-            header.splice(1, 0, <span key={0.5}>→</span>)
-          }
-
-          /* Adding 2 for padding of 1ch in each side */
-          const gutterWidth = this.maxLineNums[i] + 2;
-          const lineWidth = this.maxLineLengths[i] + 1;
-
-          const Container = styled.span`
-            margin: 20px;
-            display: block;
-            border: 1px solid silver;
-            border-radius: 3px;
-
-            .diff-hunk {
-              width: ${this.diffHunkWidth}%;
-            }
-
-            .diff-hunk-header-gutter {
-              width: ${gutterWidth * this.gutterProduct - (0.4 * this.gutterProduct)}ch;
-            }
-
-            .diff-hunk-header {
-              width: ${this.state.diffViewType == 'split' && '200%'};
-            }
-
-            .diff-gutter {
-              width: ${gutterWidth}ch;
-            }
-
-            .diff-code, .diff-hunk-header-content {
-              min-width: calc(100% - ${gutterWidth * this.gutterProduct}ch);
-              width: ${lineWidth}ch;
-            }
-
-            .diff-binary {
-              width: 100%;
-              padding: 0;
-              text-align: center;
-              font-weight: bold;
-            }
-
-            .diff-hunk-header-content {
-              width: ${lineWidth * (2 / this.gutterProduct) + gutterWidth}ch;
-            }
-          `
-
-          return (
-            <Container key={i}>
-              <DiffHeader>{header}</DiffHeader>
-              {isBinary ? (
-                <div className={`diff-binary ${newPath ? 'diff-code-insert' : 'diff-code-delete'}`}>
-                  BINARY
-                </div>
-              ) : (
-                <ReactDiffView hunks={hunks} viewType={this.state.diffViewType} />
-              )}
-            </Container>
-          )
-        })}
-      </Diff>
+      <Container ref={ref => callback(ReactDOM.findDOMNode(ref))}>
+        <DiffHeader>{header}</DiffHeader>
+        {isBinary ? (
+          <div className={`diff-binary ${newPath ? 'diff-code-insert' : 'diff-code-delete'}`}>
+            BINARY
+          </div>
+        ) : (
+          <ReactDiffView hunks={hunks} viewType={this.state.diffViewType} />
+        )}
+      </Container>
     )
   }
 }
