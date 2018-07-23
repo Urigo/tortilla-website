80d90e088b961c3b0b7a29e6831f899d18f68cc0
DAB0mB <emanor6@gmail.com>
2018-07-23 13:50:29 +0800

    Add source maps

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,14 +3,21 @@ const createPages = require('./src/startup/create-pages')
 exports.createPages = createPages
 
 exports.onCreateWebpackConfig = ({ stage, actions }) => {
-  if (stage === 'build-html') {
-    actions.setWebpackConfig({
-      loaders: [
-        {
-          test: /react-modal/,
-          loader: 'null-loader',
-        }
-      ]
-    })
+  switch (stage) {
+    case 'develop':
+      actions.setWebpackConfig({
+        devtool: 'cheap-module-source-map'
+      })
+      break
+    case 'build-html':
+      actions.setWebpackConfig({
+        loaders: [
+          {
+            test: /react-modal/,
+            loader: 'null-loader',
+          }
+        ]
+      })
+      break
   }
 }
