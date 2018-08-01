9fd269b2a852725290e93ad78a3b7418dfed447c
DAB0mB <emanor6@gmail.com>
2018-08-01 13:54:15 +0800

    Add desktop image and follow button

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,15 +1,60 @@
+import { withPrefix } from 'gatsby'
+import PropTypes from 'prop-types'
 import React from 'react'
 import styled from 'styled-components'
-import PropTypes from 'prop-types'
-import Header from './home/Header'
 import Layout from './layout'
 
-const Container = styled.div`
-  background: linear-gradient(to right, #121212, #131313, #121212);
+const DesktopImg = styled.img`
+  width: 868px;
+  height: 917px;
+  object-fit: contain;
+  position: absolute;
+  top: 0;
+  right: 0;
 `
 
-const Footer = styled.div`
-  color: white;
+const GitFollowBtn = styled.div`
+  width: 200px;
+  height: 40px;
+  padding: 3px;
+  border-radius: 5px;
+  background-color: #4c84ff;
+  box-shadow: 2px 2px 8px 0 rgba(0, 0, 0, 0.16);
+  position: absolute;
+  right: 10px;
+  top: 10px;
+  cursor: pointer;
+
+  ._icon {
+    float: left;
+    width: 34px;
+    height: 34px;
+    border-radius: 3px;
+    background-color: #3b71e8;
+
+    img {
+      display: block;
+      width: 100%;
+      height: 100%;
+      margin-left: auto;
+      margin-right: auto;
+    }
+  }
+
+  ._text {
+    float: left;
+    padding-left: 10px;
+    width: 140px;
+    height: 100%;
+    line-height: 33px;
+    font-family: Montserrat;
+    font-size: 14px;
+    font-weight: normal;
+    font-style: normal;
+    font-stretch: normal;
+    letter-spacing: normal;
+    color: #ffffff;
+  }
 `
 
 class HomePage extends React.Component {
@@ -20,10 +65,13 @@ class HomePage extends React.Component {
   render() {
     return (
       <Layout>
-        <Container>
-          <Header tutorials={this.props.tutorials} />
-          <Footer>© 2018 Uri Goldstein</Footer>
-        </Container>
+        <DesktopImg src={withPrefix('img/group-19.svg')} class="Group-19" />
+        <GitFollowBtn>
+          <div className="_icon">
+            <img src={withPrefix('icns_30/icns-30-github.svg')} alt="Github" />
+          </div>
+          <div className="_text">Follow on Github</div>
+        </GitFollowBtn>
       </Layout>
     )
   }
