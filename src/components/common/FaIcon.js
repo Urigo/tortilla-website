4fb7c344cda892258b9d690ce81ab18153622fae
DAB0mB <emanor6@gmail.com>
2018-08-09 21:44:26 +0800

    Fix incorrect use error with FontAwesome

--- src/components/common/FaIcon.js
+++ src/components/common/FaIcon.js
@@ -1,10 +1,22 @@
-import styled from 'styled-components'
+import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const getSize = ({ size }) => typeof size === 'string' ? size : size + 'px'
+const FaIcon = ({ className, size, icon, style }) => {
+  size = typeof size === 'string' ? size : `${size}px`
+  style = style || {}
 
-export default styled(FontAwesomeIcon) `
-  font-size: ${getSize};
-  line-height: ${getSize};
-  height: ${getSize};
-`
+  return (
+    <FontAwesomeIcon
+      className={className}
+      icon={icon}
+      style={{
+        fontSize: size,
+        lineHeight: size,
+        height: size,
+        ...style
+      }}
+    />
+  )
+}
+
+export default FaIcon
