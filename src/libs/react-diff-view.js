320376c9b32807a76f6ab53515474b07b4ea71d2
DAB0mB <emanor6@gmail.com>
2018-08-28 04:55:39 +0800

    Fix bug where diffs that include some completely removed or added files couldn't be parsed

--- src/libs/react-diff-view.js
+++ src/libs/react-diff-view.js
@@ -20,7 +20,28 @@ lib.parseDiff = (diffStr, options) => {
     showLong: false
   }, options)
 
-  const tooLong = diffStr.length >= 8000
+  // Fix for diff formats of new git versions where deleted and added files won't have
+  // trailing paths
+  {
+    diffStr = diffStr.trim()
+
+    const [match, aPath, bPath, fsMode] = diffStr.match(
+      /^diff --git ([^ ]+) ([^ ]+)\n([^ ]+) file mode 100644\nindex \w{7}\.\.\w{7}$/
+    ) || []
+
+    if (match) if (fsMode === 'Added') {
+      diffStr = diffStr
+        + `\n--- /dev/null`
+        + `\n+++ ${bPath}`
+    }
+    else {
+      diffStr = diffStr
+        + `\n--- ${aPath}`
+        + `\n+++ /dev/null`
+    }
+  }
+
+  const tooLong = !options.showLong && diffStr.length >= 8000
 
   // If so, get only paths metadata
   if (tooLong) {
